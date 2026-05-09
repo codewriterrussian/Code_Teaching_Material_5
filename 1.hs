0ec920014794b3c78f305112f60bc7f8ede7aa15
zxcv770880789@gmail.com <nthu106021271@gapp.nthu.edu.tw>
2026-05-09 13:11:12 +0800

    Remove unwanted root teaching materials

--- 1.hs
+++
@@ -1,8 +0,0 @@
-1. Where I am ()
-
-Normal Work Flow: 
-
-Entry Page (check ENTRY_PAGE_TEXTS) -> 
-press 领元宝 (in the Entry Page, close to +100元宝) -> 
-wait -> 
-into Search Page (check SEARCH_PAGE_TEXTS)
